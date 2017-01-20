6b0774bbed13f8cd3d3f65f1fe758e6d5e7cc4c7
Alexey <gvozdmesiter@gmail.com>
2017-01-20 16:48:20 +0300

    fix ci

--- app/controllers/api_flashcards/application_controller.rb
+++ app/controllers/api_flashcards/application_controller.rb
@@ -1,7 +1,7 @@
 module ApiFlashcards
   class ApplicationController < ActionController::Base
-
     protected
+
       def request_confirm
         authenticate_or_request_with_http_basic do |user, password|
           user == "admin" && password == "secret"
