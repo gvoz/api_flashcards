da54a9e07a611d562cd5ef832e65035f1eb0329c
Alexey <gvozdmesiter@gmail.com>
2017-01-25 14:42:34 +0300

    clear engine

--- lib/api_flashcards/engine.rb
+++ lib/api_flashcards/engine.rb
@@ -1,12 +1,5 @@
 module ApiFlashcards
   class Engine < ::Rails::Engine
     isolate_namespace ApiFlashcards
-
-    config.generators do |g|
-      g.test_framework :rspec, :fixture => false
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
-      g.assets false
-      g.helper false
-    end
   end
 end
