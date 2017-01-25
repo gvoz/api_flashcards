b1f4b6c35332a65a18d1a858a99d402011e51489
Alexey <gvozdmesiter@gmail.com>
2017-01-25 23:13:28 +0300

    fix codeclimat

--- app/controllers/api_flashcards/application_controller.rb
+++ app/controllers/api_flashcards/application_controller.rb
@@ -1,19 +1,21 @@
 module ApiFlashcards
   class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
     attr_reader :user
     before_action :request_confirm
 
     protected
 
-      def request_confirm
-        authenticate_or_request_with_http_basic do |email, password|
-          user = User.find_by(email: email)
-          if user && user.password == password
-            @user = user
-          else
-            render json: { message: "Authentication failed" }, status: 401
-          end
+    def request_confirm
+      authenticate_or_request_with_http_basic do |email, password|
+        user = User.find_by(email: email)
+        if user && user.password == password
+          @user = user
+        else
+          render json: { message: "Authentication failed" }, status: 401
         end
       end
+    end
   end
 end
