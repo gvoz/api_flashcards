b3b69e9bcb2d42c310cd7432d84680012a645d89
Alexey <gvozdmesiter@gmail.com>
2017-01-25 14:40:53 +0300

    fix apipie

--- app/controllers/api_flashcards/api/v1/cards_controller.rb
+++ app/controllers/api_flashcards/api/v1/cards_controller.rb
@@ -1,26 +1,27 @@
 module ApiFlashcards
   module Api::V1
     class CardsController < ApplicationController
+      extend Apipie::DSL::Concern
 
-      #api :GET, '/api/v1', 'Returns all user cards'
-      #error :code => 401, :desc => "Unauthorized"
+      api :GET, '/api/v1', 'Returns all user cards'
+      error :code => 401, :desc => "Unauthorized"
       def index
         @cards = @user.cards.order('review_date')
 
         render json: @cards
       end
 
-      #api :GET, '/v1/cards/:id', 'Show card'
-      #error :code => 401, :desc => "Unauthorized"
+      api :GET, '/v1/cards/:id', 'Show card'
+      error :code => 401, :desc => "Unauthorized"
       def show
         @card = Card.find(params[:id])
         render json: @card
       end
 
-      #api :POST, '/v1/cards', 'Creates new card'
-      #param :original_text, :translated_text, :review_date
-      #error :code => 400, :desc => "Wrong params: blank text"
-      #error :code => 401, :desc => "Unauthorized"
+      api :POST, '/v1/cards', 'Creates new card'
+      param :original_text, :translated_text, :review_date
+      error :code => 400, :desc => "Wrong params: blank text"
+      error :code => 401, :desc => "Unauthorized"
       def create
         @card = @user.cards.build(card_params)
 
@@ -31,9 +32,9 @@ module ApiFlashcards
         end
       end
 
-      #api :POST, '/v1/cards', 'Check Translate card'
-      #param :card_id, :user_translation
-      #error :code => 401, :desc => "Unauthorized"
+      api :POST, '/v1/cards', 'Check Translate card'
+      param :card_id, :user_translation
+      error :code => 401, :desc => "Unauthorized"
       def review
         @card = @user.cards.find(card_review_params[:card_id])
         check = CheckTranslate.new
